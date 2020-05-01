de48e0bc9ed15033408c165fb4d2f6b52ec61bf4
LABCAT <shane.watters.nz@gmail.com>
2020-05-01 12:05:34 +1200

    Adding styles for headings and paragraphs.

--- src/js/page-types/AudioProject.js
+++ src/js/page-types/AudioProject.js
@@ -4,11 +4,17 @@ import { useRouteData } from 'react-static'
 export default function AudioProject() {
     const { project } = useRouteData()
     return (
-        <div>
-            <br />
-            <h3>{project.title.rendered}</h3>
-            <p>{project.content.rendered}</p>
-            <p>{project.featuredImage}</p>
-        </div>
+        <section className="container">
+            <h1>{project.title.rendered}</h1>
+            <div>
+                
+            </div>
+            <div>
+                <br />
+
+                <p>{project.content.rendered}</p>
+                <p>{project.featuredImage}</p>
+            </div>
+        </section>
     )
 }
