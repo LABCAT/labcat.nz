18cf7841c865693a8dc845506d7fd8742a783307
LABCAT <shane.watters.nz@gmail.com>
2020-05-09 10:58:13 +1200

    Improving styling of creative coding project and implementing responsive styling for a 3 column gallery block.

--- src/js/page-types/CreativeCodingProject.js
+++ src/js/page-types/CreativeCodingProject.js
@@ -1,15 +1,16 @@
 
-import React from 'react'
-import { useRouteData } from 'react-static'
+import React from 'react';
+import { useRouteData } from 'react-static';
+
 
 export default function CreativeCodingProject() {
     const { project } = useRouteData()
     return (
-        <section className="container creative-coding-project">
+        <section className="creative-coding-project">
             <h1 dangerouslySetInnerHTML={{ __html: project.title.rendered }} >
 
             </h1>
-            <div className="creative-coding-project-content" dangerouslySetInnerHTML={{ __html: project.content.rendered }} >
+            <div className="editor-content" dangerouslySetInnerHTML={{ __html: project.content.rendered }} >
 
             </div>
         </section>
