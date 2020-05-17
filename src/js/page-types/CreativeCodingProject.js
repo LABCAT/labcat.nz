8795f2abeeccc89156bc1d2c2c563b9b59bf9d12
LABCAT <shane.watters.nz@gmail.com>
2020-05-17 19:26:32 +1200

    Implementing responsive styles for CreativeCodingProject.

--- src/js/page-types/CreativeCodingProject.js
+++ src/js/page-types/CreativeCodingProject.js
@@ -1,18 +1,29 @@
 
-import React from 'react';
+import React, { useContext, Fragment } from 'react';
 import { useRouteData } from 'react-static';
 
+import { Context } from '../context/Context.js';
 
 export default function CreativeCodingProject() {
-    const { project } = useRouteData()
+    const { windowWidth } = useContext(Context);
+    const { project } = useRouteData();
+    const showProject = windowWidth >= 992 ? true : false;
     return (
         <section className="creative-coding-project">
             <h1 dangerouslySetInnerHTML={{ __html: project.title.rendered }} >
 
             </h1>
-            <div className="editor-content" dangerouslySetInnerHTML={{ __html: project.content.rendered }} >
+            { 
+                showProject ?
+                <div className="editor-content" dangerouslySetInnerHTML={{ __html: project.content.rendered }} >
 
-            </div>
+                </div> :
+                <blockquote className="creative-coding-project-browser-message">
+                    <p>Sorry your browser needs to be at least 1000px wide to view this project.😞</p>
+                </blockquote>
+            }
         </section>
     )
 }
+
+
