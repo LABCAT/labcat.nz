333d9c0f249b4567ea13aea88c083ec047cab1e0
LABCAT <shane.watters.nz@gmail.com>
2022-02-06 18:27:51 +1300

    Adding gtm script.

--- static.config.js
+++ static.config.js
@@ -31,6 +31,15 @@ export default {
           href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500;600;800;900&family=Teko:wght@700&display=swap"
           rel="stylesheet"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KJ52WKS');`,
+          }}
+        />
       </Head>
       <div className="overlay overlay-fixed"></div>
       <Body>{children}</Body>
