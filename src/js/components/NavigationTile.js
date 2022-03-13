38d22eb3319c105866dbc9538f57d6d422a54c02
LABCAT <shane.watters.nz@gmail.com>
2022-03-13 18:28:20 +1300

    Updating layout of AnimationsHolder, styling of of H1 headings and implementing the ability for NavigationTile to open links in a new tab.

--- src/js/components/NavigationTile.js
+++ src/js/components/NavigationTile.js
@@ -16,41 +16,65 @@ const calc = (x, y) => [-(y - height / 2) / 20, (x - width / 2) / 20, 1.1];
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 export default function NavigationTile(props) {
-    const { columns, url, title, featuredImage, imagePadding, headinglevel } = props;
-    const Heading = 'h' + (headinglevel ? headinglevel : '3');
-    const [springProps, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
-    const [loaded, setLoaded] = useState(false);
+    const { columns, url, title, featuredImage, imagePadding, headinglevel, extraClasses, newTab } = props,
+        
+        [springProps, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } })),
+        
+        [loaded, setLoaded] = useState(false),
+        
+        Heading = 'h' + (headinglevel ? headinglevel : '3')
+        
+        content =  () => {
+            return (
+                <>
+                    <div 
+                        className="navigation-tile-image-holder"
+                        
+                    >
+                        {!loaded &&
+                            <Loader />
+                        }
+                        <div
+                            className={['navigation-tile-image' + (loaded ? ' loaded' : ' loading')]}
+                            style={{ backgroundImage: "url(" + featuredImage + ")", paddingBottom: imagePadding + "%" }}
+                        >
+                            {!loaded &&
+                                <img
+                                    src={featuredImage || ''}
+                                    onLoad={() => setLoaded(true)}
+                                />
+                            }
+                        </div>
+                    </div>
+                    <Heading className="navigation-tile-title" dangerouslySetInnerHTML={{ __html: title.rendered }}>
+
+                    </Heading>
+                </>
+            )
+        };
 
     return (
-        <animated.article className={[columns + ' navigation-tile' + (loaded ? ' loaded' : ' loading')]}
+        <animated.article 
+            className={
+                [
+                    columns + ' navigation-tile'  + 
+                    (extraClasses ? ' ' + extraClasses : '') +
+                    (loaded ? ' loaded' : ' loading')
+                ]
+            }
             onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
             onMouseLeave={() => set({ xys: [0, 0, 1] })}
             style={{ transform: springProps.xys.interpolate(trans) }}
         >
-            <Link to={url}>
-                <div 
-                    className="navigation-tile-image-holder"
-                    
-                >
-                    {!loaded &&
-                        <Loader />
-                    }
-                    <div
-                        className={['navigation-tile-image' + (loaded ? ' loaded' : ' loading')]}
-                        style={{ backgroundImage: "url(" + featuredImage + ")", paddingBottom: imagePadding + "%" }}
-                    >
-                        {!loaded &&
-                            <img
-                                src={featuredImage || ''}
-                                onLoad={() => setLoaded(true)}
-                            />
-                        }
-                    </div>
-                </div>
-                <Heading className="navigation-tile-title" dangerouslySetInnerHTML={{ __html: title.rendered }}>
-
-                </Heading>
-            </Link>
+            {
+                newTab ? 
+                    <a href={url} target="_blank">
+                        { content() }
+                    </a> :
+                    <Link to={url}>
+                        { content() }
+                    </Link>
+            }
         </animated.article>
     );
 }
--- src/js/page-types/AnimationsHolder.js
+++ src/js/page-types/AnimationsHolder.js
@@ -5,23 +5,28 @@ import NavigationTile from '../components/NavigationTile.js';
 export default function AnimationsHolder() {
     const { animationsPage, animationProjects } = useRouteData();
     return (
-        <section className="audio-projects-holder container">
+        <section className="animations-holder container">
             <Head><title>{animationsPage.title.rendered}</title></Head>
             <h1>{animationsPage.title.rendered}</h1>
             <div className="grid">
                 {
                     animationProjects.map(
-                        project => (
-                            <NavigationTile
-                                columns='col col-lg-2 col-xxl-3'
-                                url={`${project.animationLink}`}
-                                title={project.title}
-                                featuredImage={project.featuredImage}
-                                imagePadding="100"
-                                key={project.id}>
-                            >
-                            </NavigationTile>
-                        )
+                        (project, index) =>  {
+                            const columnClass = 'navigation-tile--column-' + ((index % 3) + 1); 
+                            return (
+                                <NavigationTile
+                                    columns='col col-lg-3'
+                                    extraClasses={columnClass}
+                                    newTab={true}
+                                    url={`${project.animationLink}`}
+                                    title={project.title}
+                                    featuredImage={project.featuredImage}
+                                    imagePadding="154.88"
+                                    key={project.id}>
+                                >
+                                </NavigationTile>
+                            )
+                        }
                     )
                 }
             </div>
